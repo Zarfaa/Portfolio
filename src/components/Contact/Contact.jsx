5415a5796752023959f1d44439878d93d6354520
Zarfaa <zarfamasood924@gmail.com>
2023-08-03 18:38:53 +0500

    Changes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,14 +3,16 @@ import "./Contact.css"
 
 let Contact = () => {
     return (
-        <section className="Contact__data" id = "contact">
-       <div className="contact__container container grid">
-           <h3 id="subtitle">Want to start a new Project?</h3>
-           <p id="section__title">Drop a message
-            <p id="section__title"><i className='bx bx-envelope'></i>zarfamasood924@gmail.com</p> </p>
-          
-       </div>
-        </section>
+        <section className="Contact__data" id="contact">
+        <div className="contact__container container grid">
+          <h3 id="subtitle">Want to start a new Project?</h3>
+          <p id="section__title">Drop a message</p>
+          <p id="section__title">
+            <i className="bx bx-envelope"></i> zarfamasood924@gmail.com
+          </p>
+        </div>
+      </section>
+      
         )
 }
 
