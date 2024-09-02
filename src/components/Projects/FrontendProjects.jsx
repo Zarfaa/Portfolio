245760fecd096610307fce1731cfbed416d6dd26
zarfaa <zarfamasood924@gmail.com>
2024-09-02 11:53:04 +0500

    removed pet vet

--- src/components/Projects/FrontendProjects.jsx
+++ src/components/Projects/FrontendProjects.jsx
@@ -2,31 +2,12 @@ import Project3 from "../../assets/react_ecommerce.png"
 import Project2 from "../../assets/project2.png"
 import Project1 from  "../../assets/project1.png"
 import Project4 from "../../assets/Project4.png"
-import Project5 from "../../assets/project5.png"
+
 let Web = () => {
 
     return(<>
      <div className="projects-container">
-        
-
-<div className="project">
-<div className="project__img">
-  <img className="image3" src={Project5} alt="Project Preview" />
-  </div>
-  <div className="project__description">
-  <h3>PetVet</h3>
-  <p>
-    PetVet is a MERN based Web application
-    <br />
-    <span>React</span> <span>Redux</span> <span>Chakra UI</span> <span>NodeJS</span>
-  </p>
-  <a href="https://petvet.ai/login" className="button" target="_blank" rel="noreferrer">
-    View Project <i class='bx bxs-right-arrow-alt'></i>
-  </a>
-  </div>
-</div>
-
-
+      
 <div className="project">
   <div className="project__description">
   <h3>Portfolio</h3>
